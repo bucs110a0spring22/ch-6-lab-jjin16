f9d6fcd021367d4f7a6e4b85336eeac96659cb23
Jiarong Jin <98533787+jjin16@users.noreply.github.com>
2022-03-28 23:55:13 +0000

    first draft

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ def seq3np1(n):
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
+    count=0
     while(n != 1):
-        print(n)
+        #print(n)
         if(n % 2) == 0:        # n is even
             n = n // 2
         else:                 # n is odd
             n = n * 3 + 1
-    print(n)                  # the last print is 1
+        count+=1
+    return count
+    #print(n)                  # the last print is 1
 
 def main():
-	seq3np1(3)
+    upper_bound= int(input("Upper bound:"))
+    start=1
+    if upper_bound<=0:
+      quit()
+    for start in range(upper_bound):
+      print(start)
+  	  count_result= seq3np1(start)
+      print(count_result)
+      start+=1
 main()
