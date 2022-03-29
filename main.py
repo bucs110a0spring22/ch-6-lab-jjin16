7aeef0a3b37cad38638e4d638b08f9d2e612873f
Jiarong Jin <98533787+jjin16@users.noreply.github.com>
2022-03-29 00:22:01 +0000

    indent problem fixed

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ def seq3np1(n):
     """
     count=0
     while(n != 1):
-        #print(n)
-        if(n % 2) == 0:        # n is even
-            n = n // 2
-        else:                 # n is odd
-            n = n * 3 + 1
+       #print(n)
+      if(n % 2) == 0:        # n is even
+        n = n // 2
+      else:                 # n is odd
+        n = n * 3 + 1
         count+=1
     return count
-    #print(n)                  # the last print is 1
+   #print(n)                  # the last print is 1
 
 def main():
-    upper_bound= int(input("Upper bound:"))
-    start=1
-    if upper_bound<=0:
-      quit()
-    for start in range(upper_bound):
-      print(start)
-  	  count_result= seq3np1(start)
-      print(count_result)
-      start+=1
+  upper_bound= int(input("Upper bound:"))
+  start=1
+  if upper_bound<=0:
+    quit()
+  for start in range(upper_bound):
+    print(start)
+    count_result=seq3np1(start)
+    print(count_result)
+    start+=1
 main()
